596ee3ba0fd491944bc09ff246ecc49dcf0e9b4a
lakyulakyu <martieszhidan@gmail.com>
2024-11-19 13:11:33 +0700

    Tugas Kafe-code pertama

---
+++ Kafe21.java
@@ -0,0 +1,18 @@
+public class Kafe21 {
+    public static void main(String[] args) {
+        menu();
+    }
+
+    public static void menu() {
+        System.out.println("\n ==MENU RESTO KAFE MARTIES");
+        System.out.println("1.Kopi Hitam - Rp.15.000,00");
+        System.out.println("2.Capucino - Rp.20.000,00");
+        System.out.println("3.Latte - Rp.22.000,00");
+        System.out.println("4.Teh Tarik - Rp.12.000,00");
+        System.out.println("5.Roti Bakar - Rp.10.000,00");
+        System.out.println("6.Mie Goreng - Rp.18.000,00");
+        System.out.println("============================");
+        System.out.println("Silahakn pilih menu yang anda inginkan");
+
+    }
+}
